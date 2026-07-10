df331a1560f17576fe71bb2654f46693fbc6b2b9
Krish Bagadia <bagadiakrish@gmail.com>
2026-07-11 00:27:21 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://ai-research-assistant-g1b1.vercel.app/"
 });
 
 export default API;

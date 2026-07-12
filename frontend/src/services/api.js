db5a62cdc5c9cae4ff31926d5a2e900a7f3b14bf
Krish Bagadia <bagadiakrish@gmail.com>
2026-07-12 15:47:28 +0530

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://ai-research-assistant-g1b1.vercel.app/"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
